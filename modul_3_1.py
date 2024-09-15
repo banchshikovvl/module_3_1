b24130cde865fd50f50897154d2059f78133c897
Vlad <banshchikovvl@yandex.ru>
2024-09-15 22:03:47 +0700

    Add files via upload

---
+++ modul_3_1.py
@@ -0,0 +1,56 @@
+
+# Пункты задачи:
+# Создать переменную calls = 0 вне функций.
+# Создать функцию count_calls и изменять в ней значение переменной calls. Эта функция должна вызываться в остальных двух функциях.
+# Создать функцию string_info с параметром string и реализовать логику работы по описанию.
+# Создать функцию is_contains с двумя параметрами string и list_to_search, реализовать логику работы по описанию.
+# Вызвать соответствующие функции string_info и is_contains произвольное кол-во раз с произвольными данными.
+# Вывести значение переменной calls на экран(в консоль).
+
+# Для использования глобальной переменной внутри функции используйте оператор global.
+# Для функции is_contains лучше привести и искомую строку и все строки в списке в один регистр.
+
+
+calls = 0
+
+
+# Функция count_calls подсчитывающая вызовы остальных функций.
+def count_calls():
+    global calls
+    calls += 1
+
+
+# Функция string_info принимает аргумент - строку и возвращает кортеж из:
+# длины этой строки, строку в верхнем регистре, строку в нижнем регистре.
+def string_info(string):
+    count_calls()
+    tuple_ = []
+    str_ = len(string)
+    str_1 = string.upper()
+    str_2 = string.lower()
+    tuple_.append(str_)
+    tuple_.append(str_1)
+    tuple_.append(str_2)
+    tuple_1 = tuple(tuple_)
+    return tuple_1
+
+
+# Функция is_contains принимает два аргумента: строку и список, и возвращает True, если строка находится в этом списке,
+# False - если отсутствует. Регистром строки при проверке пренебречь: UrbaN ~ URBAN.
+def is_contains(string, list_to_search):
+    count_calls()
+    lowercase_list = [s.lower() for s in list_to_search]
+    string_l = string.lower()
+    if string_l in lowercase_list:
+        a = True
+    else:
+        a = False
+    return a
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(string_info('Why my?1'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
+print(calls)
